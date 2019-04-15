29955b8d179583d77f2928c4c74c1347b29e75a1
DESKTOP-PM0ONBD\danie <dannymunn1995@gmail.com>
2019-04-15 12:09:10 +0100

    Added git model, controller, factory and migration

--- database/factories/GifFactory.php
+++ database/factories/GifFactory.php
@@ -11,9 +11,9 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(App\Gif::class, function (Faker\Generator $faker) {
+    
     return [
-        'name' => $faker->name,
-        'email' => $faker->email,
+        'name' => $faker->word,
     ];
 });
